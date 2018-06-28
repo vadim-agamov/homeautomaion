0dc90b888708fccbd7e4ec80bf23bf532951afaa
Vadim Agamov <vadim.agamov@gmail.com>
2018-06-28 17:20:43 +0300

    backup Thu Jun 28 17:20:43 MSK 2018

--- backup.sh
+++ backup.sh
@@ -2,6 +2,8 @@
 
 date=`date`
 
-cp -v ~/.node-red/*flow*.json .
+cd /home/vadim/code/homeautomaion
+git pull -r origin master
+cp -v /home/vadim/code/homeautomaion/.node-red/*flow*.json .
 git add .
 git commit -m "backup $date" && git push origin master || echo "no changes"
